ce15dc3adae47b5d9d88ef373034c357242c6bad
Nandan <nandanmarkrao@gmail.com>
2020-02-21 17:02:14 +0100

    botspine 0.0.9, changed chunkedTransform for transform

--- naughtywriter.js
+++ naughtywriter.js
@@ -3,14 +3,13 @@ const Chatbase = require('@vlab-research/chatbase-postgres')
 const {writeWaits, writeTimeouts} = require('./lib/writer')
 const {pipeline} = require('stream')
 
+
 const spine = new BotSpine('naughtybot')
 const pool = new Chatbase().pool
 
-const chunkSize = +process.env.NAUGHTY_CHUNK_SIZE || 100
-const timeout = +process.env.NAUGHTY_TIMEOUT || 5000
 
 pipeline(spine.source(),
-         spine.chunkedTransform(async (m) => {
+         spine.transform(async (m) => {
 
            // Write waits and timeouts concurrently
            await Promise.all([writeWaits(pool, m),
