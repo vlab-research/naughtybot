f02f828ed4c40aee38b8e0d20413126024400072
Nandan <nandanmarkrao@gmail.com>
2020-02-21 16:48:11 +0100

    fixed bug with stuck pipe due to improper use of transforms

--- naughtywriter.js
+++ naughtywriter.js
@@ -10,7 +10,11 @@ const chunkSize = +process.env.NAUGHTY_CHUNK_SIZE || 100
 const timeout = +process.env.NAUGHTY_TIMEOUT || 5000
 
 pipeline(spine.source(),
-         spine.chunkedTransform(m => writeWaits(pool, m), chunkSize, timeout),
-         spine.chunkedTransform(m => writeTimeouts(pool, m), chunkSize, timeout),
+         spine.chunkedTransform(async (m) => {
+
+           // Write waits and timeouts concurrently
+           await Promise.all([writeWaits(pool, m),
+                              writeTimeouts(pool, m)])
+         }, chunkSize, timeout),
          spine.sink(),
          err => console.error(err))
